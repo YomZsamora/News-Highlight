259a9e06aab5476b55308c244ce8b7261926a017
yomz <samora.y@adzumi.co.ke>
2018-12-31 19:23:01 +0300

    Installed Bootstrap via Pip.

--- app/views.py
+++ app/views.py
@@ -8,4 +8,5 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title)
