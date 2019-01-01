38f1f2eaabd8f223d4bce116c10e9feb92fd5b5b
yomz <samora.y@adzumi.co.ke>
2019-01-01 11:53:26 +0300

    Start of request to API set up.

--- app/request.py
+++ app/request.py
@@ -1,4 +1,11 @@
 from app import app
+import urllib.request,json
+from .models import news
+
+News = news.News
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+# Getting the news base url
+base_url = app.config["NEWS_HIGHLIGHTS_BASE_URL"]
